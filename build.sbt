ba6b3365da5edbff6a3190110eed8512e2fdf815
saurabhritu <firstrankforall@gmail.com>
2021-10-12 22:23:32 +0530

    added a simple sparktest App

--- build.sbt
+++ build.sbt
@@ -2,9 +2,9 @@ name := "Aspafka"
 
 version := "0.1"
 
-scalaVersion := "2.13.6"
-//
-//libraryDependencies ++= Seq(
-//  "org.apache.spark" %% "spark-core" % "3.1.2",
-//  "org.apache.spark" %% "spark-sql" % "3.1.2"
-//)
+scalaVersion := "2.12.10"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "3.1.2",
+  "org.apache.spark" %% "spark-sql" % "3.1.2"
+)
---
+++ src/main/scala/sparkTest.scala
@@ -0,0 +1,13 @@
+/* sparkTest.scala */
+import org.apache.spark.sql.SparkSession
+object sparkTest {
+  def main(args: Array[String]) {
+    val logFile = "/Desktop/SR_To_Do-list.txt" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}
