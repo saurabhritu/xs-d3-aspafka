f090afa0fd57b500a98d6afee78ddf5afec6b556
saurabhritu <firstrankforall@gmail.com>
2021-10-15 21:47:58 +0530

    spark data set & frame program v.1

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "Aspafka"
 
-version := "0.1"
+version := "1.0"
 
 scalaVersion := "2.12.10"
 
---
+++ src/main/scala/Spark_DSF.scala
@@ -0,0 +1,17 @@
+// imports
+import org.apache.spark.sql.SparkSession
+
+object Spark_DSF {
+
+  def main(args:Array[String]){
+    val spark = SparkSession
+      .builder()
+      .appName("Spark_DSF")
+      .config("spark.master", "local")
+      .getOrCreate()
+    // Becareful about json structure
+    val df = spark.read.json("/home/saurabh/Desktop/Spafka_RW/Spark_DSF_RW/DSF_1.json")
+    df.show()
+  }
+
+}
