e99b94a787fa07424e9f1122690b7d6311ad63c7
saurabhritu <firstrankforall@gmail.com>
2021-10-15 22:07:25 +0530

    minor comments

--- src/main/scala/Spark_DSF.scala
+++ src/main/scala/Spark_DSF.scala
@@ -9,9 +9,8 @@ object Spark_DSF {
       .appName("Spark_DSF")
       .config("spark.master", "local")
       .getOrCreate()
-    // Becareful about json structure
+    // Becareful about json file structure
     val df = spark.read.json("/home/saurabh/Desktop/Spafka_RW/Spark_DSF_RW/DSF_1.json")
     df.show()
   }
-
 }
